6c777505b07729abc8710f1c0a68bcaddf5bf56b
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-05-31 08:12:16 +0200

    endpoint POST - creating a new user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { db } from './database/knex'
+import { TUserDB } from './types'
 
 const app = express()
 
@@ -8,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+    console.log(`Server running at port ${3003}`)
 })
 
 app.get("/ping", async (req: Request, res: Response) => {
@@ -24,7 +25,7 @@ app.get("/ping", async (req: Request, res: Response) => {
         if (error instanceof Error) { 
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -52,7 +53,85 @@ app.get("/user",async (req: Request, res: Response)=>{
         if (error instanceof Error){
             res.send(error.message)
         }else {
-            res.send("Error inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
+
+app.post("/user", async (req: Request, res: Response) => {
+    try{
+        const {id, name, email, password} = req.body //como deve ser o corpo da chamada para criar um novo usuário
+        
+        //validações 
+        if (typeof id !== "string"){
+            res.status(400)
+            throw new Error("'id' should be a string")
+        }
+
+        if (id.length < 4){
+            res.status(400)
+            throw new Error("'id' should be more then 4 characters")
+        }
+
+        if (typeof name !== "string"){
+            res.status(400)
+            throw new Error("'name' should be a string")
+        }
+
+        if (name.length < 2){
+            res.status(400)
+            throw new Error("'name' should be more then 2 characters")
+        }
+
+        if (typeof email !== "string"){
+            res.status(400)
+            throw new Error("'e-mail' should be a string")
+        }
+
+        if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/g)) {
+			throw new Error("'password' should be in between 8 e 12 characters, with uppercase and lowercase letters and at list one number and one special character")
+		}
+
+        const [ userIdAlreadyExists ]: TUserDB[] | undefined[] = await db("user").where({id})
+        
+        if (userIdAlreadyExists) {
+            res.status(400)
+            throw new Error ("'id' already exists")
+        }
+
+        const [ userEmailAlreadyExists ]: TUserDB[] | undefined[] = await db("user").where({email})
+        
+        if (userEmailAlreadyExists) {
+            res.status(400)
+            throw new Error ("'e-mail' already exists")
+        }
+
+        const newUser: TUserDB = {
+            id,
+            name,
+            email,
+            password
+        }
+
+        await db("user").insert(newUser)
+        res.status(201).send({
+            message: "User created successfully",
+            user: newUser
+        })  
+
+    }
+    catch(error){
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Unexpected Error")
+        }
+    }
+})
+
